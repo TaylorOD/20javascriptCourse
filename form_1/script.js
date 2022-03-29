524da41d469bf1cc53bfd89b23640d70b6579ce3
taylorOD <taylorsunwantedemails@gmail.com>
2022-03-29 16:12:06 -0700

    add better validations

--- form_1/script.js
+++ form_1/script.js
@@ -22,34 +22,50 @@ const isValidEmail = (email) => {
   return re.test(String(email).toLocaleLowerCase());
 }
 
+const checkRequired = (inputArray) => {
+  inputArray.forEach(function(input) {
+    if(input.value.trim() === "") {
+      showError(input, `${getFieldName(input)} is required`);
+    } else {
+      showSuccess(input);
+    }
+  });
+};
+
+const getFieldName = (input) => {
+  return input.id[0].toUpperCase() + input.id.slice(1);
+};
 
 form.addEventListener('submit', function(e) {
   e.preventDefault();
 
-  if (username.value === '') {
-    showError(username, "Username is Required");
-  } else {
-    showSuccess(username);
-  }
-
-  if (email.value === '') {
-    showError(email, "Email is Required");
-  } else if (!isValidEmail(email.value)) {
-    showError(email, "Email is not valid");
-	} else {
-		showSuccess(email);
-	}
+  checkRequired([username, email, password, password2])
   
-  if (password.value === '') {
-    showError(password, "Password is Required");
-  } else {
-    showSuccess(password);
-  }
+  // Old Validations Below:
+  // if (username.value === '') {
+  //   showError(username, "Username is Required");
+  // } else {
+  //   showSuccess(username);
+  // }
 
-  if (password2.value === '') {
-    showError(password2, "Confirm password is the same as above");
-  } else {
-    showSuccess(password2);
-  }
+  // if (email.value === '') {
+  //   showError(email, "Email is Required");
+  // } else if (!isValidEmail(email.value)) {
+  //   showError(email, "Email is not valid");
+	// } else {
+	// 	showSuccess(email);
+	// }
+  
+  // if (password.value === '') {
+  //   showError(password, "Password is Required");
+  // } else {
+  //   showSuccess(password);
+  // }
+
+  // if (password2.value === '') {
+  //   showError(password2, "Confirm password is the same as above");
+  // } else {
+  //   showSuccess(password2);
+  // }
   
 });
