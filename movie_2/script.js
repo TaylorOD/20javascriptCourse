07b3fc2fb5d05677223b5b19853f552ab8e54c80
taylorOD <taylorsunwantedemails@gmail.com>
2022-04-08 09:43:16 -0700

    add local storage calls

--- movie_2/script.js
+++ movie_2/script.js
@@ -8,15 +8,27 @@ let ticketPrice = +movieSelect.value;
 
 const updateSelectedCount = () => {
   const selectedSeats = document.querySelectorAll('.row .seat.selected');
+  
+  const seatsIndex = [...selectedSeats].map((seat) => {
+    return [...seats].indexOf(seat);
+  });
+  
+  localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+  
   const selectedSeatsCount = selectedSeats.length;
-
   count.innerText = selectedSeatsCount;
   total.innerText = selectedSeatsCount * ticketPrice;
 };
 
+const selectMovieData = (movieIndex, ticketPrice) => {
+  localStorage.setItem('selectedMovieIndex', movieIndex);
+  localStorage.setItem('selectedMoviePrice', ticketPrice);
+}
+
 // Movie Select Event
 movieSelect.addEventListener('change', (e) => {
   ticketPrice = e.target.value;
+  selectMovieData();
   updateSelectedCount();
 });
 
