bf20a11ef58a57d908eda9e5438a298bbfc6616f
taylorOD <taylorsunwantedemails@gmail.com>
2022-04-10 12:29:51 -0700

    add find js for movie selector

--- movie_2/script.js
+++ movie_2/script.js
@@ -2,7 +2,9 @@ const container = document.querySelector('.container');
 const seats = document.querySelectorAll('.row .seat:not(.occupied)');
 const count = document.getElementById('count');
 const total = document.getElementById('total');
-const movieSelect = document.getElementById('movie')
+const movieSelect = document.getElementById('movie');
+
+populateUI();
 
 let ticketPrice = +movieSelect.value;
 
@@ -23,12 +25,12 @@ const updateSelectedCount = () => {
 const selectMovieData = (movieIndex, ticketPrice) => {
   localStorage.setItem('selectedMovieIndex', movieIndex);
   localStorage.setItem('selectedMoviePrice', ticketPrice);
-}
+};
 
 // Movie Select Event
 movieSelect.addEventListener('change', (e) => {
-  ticketPrice = e.target.value;
-  selectMovieData();
+  ticketPrice = +e.target.value;
+  selectMovieData(e.target.selectedIndex, e.target.value);
   updateSelectedCount();
 });
 
@@ -41,3 +43,22 @@ container.addEventListener('click', (e) => {
     updateSelectedCount();
   }
 });
+
+function populateUI() {
+  const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add('selected');
+      }
+    });
+  }
+
+  const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+  if (selectedMovieIndex !== null) {
+    movieSelect.selectedIndex = selectedMovieIndex;
+  }
+};
+
+updateSelectedCount();
