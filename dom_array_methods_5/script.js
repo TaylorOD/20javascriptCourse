8e08df71692d1d205528910781d77d705ea689de
taylorOD <taylorsunwantedemails@gmail.com>
2022-04-25 10:31:42 -0700

    add for each

--- dom_array_methods_5/script.js
+++ dom_array_methods_5/script.js
@@ -21,9 +21,32 @@ async function getRandomUser() {
     name: `${user.name.first} ${user.name.last}`,
     money: Math.floor(Math.random() * 1000000)
   }
+  addData(newUser)
 }
 
 // Add new obj to data arr
 function addData(obj) {
   data.push(obj)
+  updateDOM()
 }
+
+// Update DOM
+function updateDOM(providedData = data) {
+  // Clear main div
+  main.innerHTML = '<h2><strong>Person</strong>Wealth</h2>'
+
+  providedData.forEach((item) => {
+    const element = document.createElement('div')
+    element.classList.add('person')
+    element.innerHTML = `<strong>${item.name}</strong> ${formatMoney(item.money)}`
+    main.appendChild(element)
+  })
+}
+
+// Format number as money
+function formatMoney(number) {
+  return '$' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+}
+
+// Event Listens
+addUserBtn.addEventListener('click', getRandomUser)
