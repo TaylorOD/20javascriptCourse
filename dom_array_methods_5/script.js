15570a185ff289db56bd9ecd1fcf8e6750d406e5
taylorOD <taylorsunwantedemails@gmail.com>
2022-04-28 09:46:53 -0700

    add reduce and filter

--- dom_array_methods_5/script.js
+++ dom_array_methods_5/script.js
@@ -38,6 +38,19 @@ function sortByRichest() {
   updateDOM();
 }
 
+// Filter only millionaires
+function showMillionaires() {
+  data = data.filter(user => user.money >= 1000000)
+  updateDOM()
+}
+
+function calculateWealth() {
+  const wealth = data.reduce((total, user) => (total += user.money), 0)
+  const wealthEl = document.createElement('div')
+  wealthEl.innerHTML = `<h3>Total Wealth: <strong>${formatMoney(wealth)}</strong></h3>`
+  main.appendChild(wealthEl)
+}
+
 // Add new obj to data arr
 function addData(obj) {
   data.push(obj)
@@ -67,3 +80,5 @@ function formatMoney(number) {
 addUserBtn.addEventListener('click', getRandomUser)
 doubleBtn.addEventListener('click', doubleMoney)
 sortBtn.addEventListener('click', sortByRichest)
+showMillionairesBtn.addEventListener('click', showMillionaires)
+calculateWealthBtn.addEventListener('click', calculateWealth)
