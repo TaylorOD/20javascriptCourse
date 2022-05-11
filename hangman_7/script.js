f94dc2cc241de57abb99d4dbe405bf2f2d13f57e
taylorOD <taylorsunwantedemails@gmail.com>
2022-05-11 10:10:53 -0700

    add final javacript for hangman project

--- hangman_7/script.js
+++ hangman_7/script.js
@@ -1,6 +1,6 @@
 const wordEl = document.getElementById('word')
 const wrongLettersEl = document.getElementById('wrong-letters')
-const playAgainBtn = document.getElementById('play-again')
+const playAgainBtn = document.getElementById('play-button')
 const popup = document.getElementById('popup-container')
 const notification = document.getElementById('notification-container')
 const finalMessage = document.getElementById('final-message')
@@ -35,7 +35,25 @@ function displayWord() {
 
 // Update wrong letters element 
 function updateWrongLettersEl() {
-  console.log("wrong letter")
+  // Display wrong letters
+  wrongLettersEl.innerHTML = `
+  ${wrongLetters.length > 0 ? '<p>Wrong</p>' : ''}
+  ${wrongLetters.map(letter => `<span>${letter}</span>`)}
+  `
+  // Display body parts
+  figureParts.forEach((part, index) => {
+    const errors = wrongLetters.length
+    if (index < errors) {
+      part.style.display = 'block'
+    } else {
+      part.style.display = 'none'
+    }
+  })
+  // Check if lost
+  if (wrongLetters.length === figureParts.length) {
+    finalMessage.innerText = 'Unfortunately, you lost!'
+    popup.style.display = 'flex'
+  }
 }
 
 // Show notification
@@ -69,6 +87,20 @@ window.addEventListener('keydown', e => {
       }
     }
   }
+})
+
+// Play again button press
+playAgainBtn.addEventListener('click', () => {
+  // empty arrays
+  correctLetters.splice(0)
+  wrongLetters.splice(0)
+
+  selectedWord = words[Math.floor(Math.random() * words.length)]
+
+  displayWord()
+
+  updateWrongLettersEl()
+  popup.style.display = 'none'
 })
 
 displayWord()
