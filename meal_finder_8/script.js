83033a47d83962bc0deb42c774ca275b7da4cb68
taylorOD <taylorsunwantedemails@gmail.com>
2022-05-16 10:52:36 -0700

    add javascript and html

--- meal_finder_8/script.js
+++ meal_finder_8/script.js
@@ -19,7 +19,6 @@ function searchMeal(e) {
     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
       .then(response => response.json())
       .then(data => {
-        console.log(data)
         resultHeading.innerHTML = `<h2>Search Results for '${term}':</h2>`
 
         if (data.meals === null) {
@@ -42,5 +41,60 @@ function searchMeal(e) {
   }
 }
 
+// Fetch meal by ID
+function getMealById(mealID) {
+  fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`)
+    .then(res => res.json())
+    .then(data => {
+      const meal = data.meals[0]
+      addMealToDOM(meal)
+    });
+}
+
+// Add meal to DOM
+function addMealToDOM(meal) {
+  const ingredients = []
+
+  for (let index = 1; index <= 20; index += 1) {
+    if (meal[`strIngredient${index}`]) {
+      ingredients.push(`${meal[`strIngredient${index}`]} - ${meal[`strMeasure${index}`]}`)
+    } else {
+      break
+    }
+  }
+  single_mealEl.innerHTML = `
+    <div class="single-meal">
+      <h1>${meal.strMeal}</h1>
+      <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
+      <div class="single-meal-info">
+        ${meal.strCategory ? `<p>${meal.strCategory}</p>` : ''}
+        ${meal.strArea ? `<p>${meal.strArea}</p>` : ''}
+      </div>
+      <div class="main">
+        <p>${meal.strInstructions}</p>
+        <h2>Ingredients</h2>
+        <ul>
+          ${ingredients.map(ing => `<li>${ing}</li>`).join('')}
+        </ul>
+      </div>
+    </div>
+  `
+  
+}
+
 // Event Listeners
 submit.addEventListener('submit', searchMeal)
+
+mealsEl.addEventListener('click', e => {
+  const mealInfo = e.path.find(item => {
+    if (item.classList) {
+      return item.classList.contains('meal-info')
+    } else {
+      return false
+    }
+  })
+  if (mealInfo) {
+    const mealID = mealInfo.getAttribute('data-mealid')
+    getMealById(mealID)
+  }
+})
