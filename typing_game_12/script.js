a7abf4104b9d92b793813fb04f7251283cf19f2a
taylorOD <taylorsunwantedemails@gmail.com>
2022-07-19 12:45:51 -0600

    finish typing game and add speech reader set up

--- typing_game_12/script.js
+++ typing_game_12/script.js
@@ -38,6 +38,12 @@ let score = 0;
 
 let time = 10;
 
+// Set difficulty to value in local storage or medium
+let difficulty = localStorage.getItem('difficulty') !== null ? localStorage.getItem('difficulty') : 'medium';
+
+// Set difficulty select value
+difficultySelect.value = localStorage.getItem('difficulty') !== null ? localStorage.getItem('difficulty') : 'medium';
+
 // Focus on text on start
 text.focus()
 
@@ -97,6 +103,24 @@ text.addEventListener('input', e => {
 
     time += 5;
 
+    if (difficulty === 'hard') {
+      time += 2
+    } else if (difficulty === 'medium') {
+      time += 3
+    } else {
+      time += 5
+    }
+
     updateTime()
   }
+})
+
+// Settings btn click
+settingsBtn.addEventListener('click', () => 
+settings.classList.toggle('hide'))
+
+// Settings select 
+settingsForm.addEventListener('change', e => {
+  difficulty = e.target.value;
+  localStorage.setItem('difficulty', difficulty);
 })
