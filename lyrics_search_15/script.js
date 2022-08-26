6622225103f893b7065393cdf69e3c085b67938c
taylorOD <taylorsunwantedemails@gmail.com>
2022-08-26 09:59:48 -0600

    finish js for project

--- lyrics_search_15/script.js
+++ lyrics_search_15/script.js
@@ -68,3 +68,25 @@ form.addEventListener('submit', (e) => {
 		searchSongs(searchTerm);
 	}
 });
+
+async function getLyrics(artist, songTitle) {
+	const res = await fetch(`${apiURL}/v1/${artist}/${songTitle}`);
+	const data = await res.json();
+
+	const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, '<br>');
+	result.innerHTML = `<h2><strong>${artist}</strong> - ${songTitle}</h2>
+	<span>${lyrics}</span>`;
+
+	more.innerHTML = '';
+}
+
+// Get lyrics button click
+result.addEventListener('click', (e) => {
+	const clickedEl = e.target;
+	if (clickedEl.tagName === 'BUTTON') {
+		const artist = clickedEl.getAttribute('data-artist');
+		const songTitle = clickedEl.getAttribute('data-songtitle');
+
+		getLyrics(artist, songTitle);
+	}
+});
