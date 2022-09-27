cf6a6e736ffcbea9c55123c682b63e234308b356
taylorOD <taylorsunwantedemails@gmail.com>
2022-09-27 11:59:30 -0600

    finish new year countdown

--- new_year_countdown_18/script.js
+++ new_year_countdown_18/script.js
@@ -3,10 +3,16 @@ const hours = document.getElementById('hours');
 const minutes = document.getElementById('minutes');
 const seconds = document.getElementById('seconds');
 const countdown = document.getElementById('countdown');
+const year = document.getElementById('year');
+const loading = document.getElementById('loading');
 
 const currentYear = new Date().getFullYear();
 const newYearTime = new Date(`January 01 ${currentYear + 1} 00:00:00`);
 
+// Set background year
+year.innerText = currentYear + 1;
+
+// Update countdown time
 function updateCountdown() {
 	const currentTime = new Date();
 	const diff = newYearTime - currentTime;
@@ -16,6 +22,7 @@ function updateCountdown() {
 	const convertedMinutes = Math.floor(diff / 1000 / 60) % 60;
 	const convertedSeconds = Math.floor(diff / 1000) % 60;
 
+	// Add values to down
 	days.innerHTML = convertedDays;
 	hours.innerHTML = convertedHours < 10 ? '0' + convertedHours : convertedHours;
 	minutes.innerHTML =
@@ -24,4 +31,11 @@ function updateCountdown() {
 		convertedSeconds < 10 ? '0' + convertedSeconds : convertedSeconds;
 }
 
+// Show spinner before countdown
+setTimeout(() => {
+	loading.remove();
+	countdown.style.display = 'flex';
+}, 1000);
+
+// Run every seconds
 setInterval(updateCountdown, 1000);
