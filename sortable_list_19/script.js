99088362d983f44d16e4af5e9c64c2319fd8d08c
taylorOD <taylorsunwantedemails@gmail.com>
2022-09-29 09:28:33 -0600

    add set up for sortable list project

---
+++ sortable_list_19/script.js
@@ -0,0 +1,41 @@
+const draggable_list = document.getElementById('draggable-list');
+const check = document.getElementById('check');
+
+const richestPeople = [
+	'jeff Bezos',
+	'Bill Gates',
+	'Warren Buffet',
+	'Bernard Arnault',
+	'Carlos Slim Helu',
+	'Amancio Ortega',
+	'Larry Ellison',
+	'Mark Zuckerberg',
+	'Michael Bloomberg',
+	'Larry Page',
+];
+
+// Store list items
+const listItems = [];
+
+let dragStartIndex;
+
+// Insert list items into DOM
+function createList() {
+	[...richestPeople].forEach((person, index) => {
+		const listItem = document.createElement('li');
+		listItem.setAttribute('data-index', index);
+		listItem.innerHTML = `
+    <span class="number">${index + 1}</span>
+    <div class="draggable" draggable="true">
+      <p class="person-name">${person}</p>
+      <i class="fas fa-grip-lines"></i>
+    </div>
+    `;
+
+		listItems.push(listItem);
+
+		draggable_list.appendChild(listItem);
+	});
+}
+
+createList();
