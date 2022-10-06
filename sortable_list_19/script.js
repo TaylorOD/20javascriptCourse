f0036ccfbf782f5f402cd5a8aa016fbb170d4c5e
taylorOD <taylorsunwantedemails@gmail.com>
2022-10-06 11:08:41 -0600

    add javascript to move and swap

--- sortable_list_19/script.js
+++ sortable_list_19/script.js
@@ -42,6 +42,52 @@ function createList() {
 
 			draggable_list.appendChild(listItem);
 		});
+
+	addEventListeners();
+}
+
+function dragStart() {
+	dragStartIndex = +this.closest('li').getAttribute('data-index');
+}
+function dragEnter() {
+	this.classList.add('over');
+}
+function dragLeave() {
+	this.classList.remove('over');
+}
+
+function dragDrop() {
+	const dragEndIndex = +this.getAttribute('data-index');
+	swapItems(dragStartIndex, dragEndIndex);
+
+	this.classList.remove('over');
+}
+function dragOver(e) {
+	e.preventDefault();
+}
+
+function swapItems(fromIndex, toIndex) {
+	const itemOne = listItems[fromIndex].querySelector('.draggable');
+	const itemTwo = listItems[toIndex].querySelector('.draggable');
+
+	listItems[fromIndex].appendChild(itemTwo);
+	listItems[toIndex].appendChild(itemOne);
+}
+
+function addEventListeners() {
+	const draggables = document.querySelectorAll('.draggable');
+	const dragListItems = document.querySelectorAll('.draggable-list li');
+
+	draggables.forEach((item) => {
+		item.addEventListener('dragstart', dragStart);
+	});
+
+	dragListItems.forEach((item) => {
+		item.addEventListener('dragover', dragOver);
+		item.addEventListener('drop', dragDrop);
+		item.addEventListener('dragenter', dragEnter);
+		item.addEventListener('dragleave', dragLeave);
+	});
 }
 
 createList();
